fcd81a75dd6224f2727973d9231e26e775211b8a
akfangus <akfanakfan@naver.com>
2023-01-17 06:07:40 +0900

    small mistake

--- static/js/member.js
+++ static/js/member.js
@@ -1,27 +1,26 @@
 $(document).ready(function () {
-
-    listening()
+  listening();
 });
 
-
 function listening() {
+  $.ajax({
+    type: "GET",
+    url: "/UserList/call",
+    data: {},
+    success: function (response) {
+      let rows = response["user_call"];
+      for (let i = 0; i < rows.length; i++) {
+        let name = rows[i]["name"];
+        let img = rows[i]["img"];
+        let comment = rows[i]["comment"];
+        let id = rows[i]["id"];
 
-    $.ajax({
-        type: "GET",
-        url: "/UserList/call",
-        data: {},
-        success: function (response) {
-            let rows = response['user_call']
-            for (let i = 0; i < rows.length; i++) {
-                let name = rows[i]['name']
-                let img = rows[i]['img']
-                let comment = rows[i]['comment']
-
-                let temp_html = ` <div class="card">
+        let temp_html = ` <div class="card">
                                     <div>
                                         <img src="${img}" class="profile"/>
                                     </div>
                                     <div class="user-info">
+                                        <h2>${id}<h2>
                                         <h2>${name}</h2>
                                         <div class="comment">
                                             ${comment}
@@ -30,24 +29,10 @@ function listening() {
                                            <button type="button" class="bookmk" style=cursor:pointer;>⭐bookmark</button>
                                         </div>
                                     </div>
-                                   </div> `
+                                   </div> `;
 
-                $('#user-card').append(temp_html)
-            }
-        }
-
-    })
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
+        $("#user-card").append(temp_html);
+      }
+    },
+  });
+}
