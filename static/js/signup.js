547fd2f4bca81dd05af71c46e9cf7868c5d048f4
LIHA <lihaoka27@gmail.com>
2023-01-18 01:09:55 +0900

    feat: signup.html 추가, signup.js 수정, signup.css 수정

--- static/js/signup.js
+++ static/js/signup.js
@@ -1,56 +1,68 @@
+    function signup() {
+        let signup_id = $('#id').val()
+        let signup_name = $('#name').val()
+        let signup_pwd = $('#passwd').val()
+        let signup_pwd2 = $('#passwd2').val()
+        let gitcomment = $('#gitcomment').val()
+        let gitimage = $('#image').val()
 
-      // 회원가입 란 함수 - 공백이 있을 시 경고 띄우려고 해요. alert 대신 토스트 메시지 하고싶은뎅
-
-      function signup() {
-            let id = $('#id').val()
-            let name = $('#name').val()
-            let pwd = $('#passwd').val()
-            let comment = $('#comment').val()
-            let image = $('#image')
-
-            $.ajax({
-                type: 'POST',
-                url: '/signup',
-                data: {
-                    id_give: id,
-                    name_give: name,
-                    pwd_give: pwd,
-                    comment_give: comment,
-                    image_give: image
-                    },
-                success: function (response) {
-
-                    if (signup_id.replace(/\s| /gi, "").length == 0) {
-                        alert("아이디는 공백일 수 없습니다");
-                        $('#yourid').focus();
-                    }
-                    if (signup_name.replace(/\s| /gi, "").length == 0) {
-                        alert("이름 공백일 수 없습니다");
-                        $('#name').focus();
-                    }
-                    if (signup_pwd.replace(/\s| /gi, "").length == 0) {
-                        alert("비밀번호는 공백일 수 없습니다");
-                        $('#comment').focus();
-                    }
-                    if (comment.replace(/\s| /gi, "").length == 0) {
-                        alert("코멘트를 입력해 주세요!");
-                        $('#signup_pwd').focus();
-                    }
-                    else {
-                     alert("회원 가입이 완료되었습니다.")
-                    }
-                    window.location.reload()
-                }
-            });
+        if (signup_id.trim() == 0 || signup_id.trim() == null) {
+            alert("아이디는 공백일 수 없습니다");
+            $('#id').focus();
+            return;
+        }
+        if (signup_name.trim() == 0 || signup_name.trim() == null) {
+            alert("이름은 공백일 수 없습니다");
+            $('#name').focus();
+            return;
+        }
+        if (signup_pwd.trim() == 0 || signup_pwd.trim() == null) {
+            alert("비밀번호는 공백일 수 없습니다");
+            $('#passwd').focus();
+            return;
+        }
+        if (signup_pwd2.trim() == 0 || signup_pwd2.trim() == null) {
+            alert("비밀번호 확인을 해주세요!");
+            $('#passwd2').focus();
+            return;
+        }
+        if (signup_pwd != '' && signup_pwd2 == '' ) {
+            null;
+        } else if (signup_pwd != "" || signup_pwd2 != "") {
+            if (signup_pwd == signup_pwd2) {
+            } else {
+                alert("비밀번호가 일치하지 않아요!");
+                $('#passwd2').focus();
+                return;
+            }
+        }
+        if (gitcomment.trim() == 0 || gitcomment.trim() == null) {
+            alert("코멘트를 입력해 주세요!");
+            $('#gitcomment').focus();
+            return;
         }
 
-            $("signupBtn").click(function(){
-                  toastr.options = {
-                      closeButton: true,
-                      progressBar: true,
-                      showMethod: 'slideDown',
-                      timeOut: 4000
-                  };
-                  toastr.success('www.leafcats.com', 'Toastr success!');
-    });
+        console.log(signup_id)
+        console.log(signup_name)
+        console.log(signup_pwd)
+        console.log(signup_pwd2)
+        console.log(gitcomment)
+        console.log(gitimage)
 
+        $.ajax({
+            type: 'POST',
+            url: '/signup',
+            data: {
+                account_give: signup_id,
+                name_give: signup_name,
+                pwd_give: signup_pwd,
+                comment_give: gitcomment,
+                image_give: gitimage
+            },
+
+            success: function (response) {
+                alert(response['msg'])
+                location.href="/login"
+            }
+        });
+    }
